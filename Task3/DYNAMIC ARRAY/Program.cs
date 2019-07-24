407edd342dc9d8e350729a79008a16410b4c393a
Andriyanov Denis <andriyanovdenis99@gmail.com>
2019-07-24 14:59:20 +0400

    Completed the dynamic array

--- Task3/DYNAMIC ARRAY/Program.cs
+++ Task3/DYNAMIC ARRAY/Program.cs
@@ -9,13 +9,58 @@ namespace DYNAMIC_ARRAY
 		static void Main(string[] args)
 		{
 			DynamicArray<int> array = new DynamicArray<int>();
-			array.Add(10);
-			List<int> vs = new List<int>{ 0, 1, 2, 3, 4 };
+
+			//Вставка IEnumerable
+			List<int> vs = new List<int> { 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4 };
 			array.AddRange(vs);
-			for (int i = 0; i < array.Length; i++)
+
+			//Вывод по индексу
+			Console.WriteLine("Вывод по индексу 0: {0}", array[0]);
+
+			//Вывод foreach
+			Console.Write("Вывод foreach: ");
+			foreach (int x in array)
 			{
-				Console.WriteLine(array[i]);
+				Console.Write(x + " ");
 			}
+			Console.WriteLine();
+
+			//Вставка по индексу
+			array.Insert(999, 3);
+			Console.Write("Вставка в позицию 3 числа 999: ");
+			foreach (int x in array)
+			{
+				Console.Write(x + " ");
+			}
+			Console.WriteLine();
+
+			//Удаление
+			Console.Write("Введите число для удаления: ");
+			if (!int.TryParse(Console.ReadLine(), out int res))
+			{
+				Console.WriteLine("Неверно введено число.");
+				return;
+			}
+			if (!array.Remove(res))
+				Console.WriteLine("Число не найдено.");
+			else
+			{
+				Console.Write("Удаление числа {0}: ", res);
+				foreach (int x in array)
+				{
+					Console.Write(x + " ");
+				}
+				Console.WriteLine();
+			}
+
+			//Создание массива из IEnumerable
+			DynamicArray<int> vs1 = new DynamicArray<int>(vs);
+			Console.Write("Создание массива из IEnumerable: ");
+			foreach (int x in vs1)
+			{
+				Console.Write(x + " ");
+			}
+			Console.WriteLine();
 		}
 
 	}
@@ -71,7 +116,8 @@ namespace DYNAMIC_ARRAY
 
 		public DynamicArray()
 		{
-			array = new T[8];
+			int initCapacity = 8;
+			array = new T[initCapacity];
 			Length = 0;
 		}
 
@@ -80,6 +126,28 @@ namespace DYNAMIC_ARRAY
 			array = new T[capacity];
 			Length = 0;
 		}
+
+		public DynamicArray(IEnumerable<T> ts)
+		{
+			int additCapacity = 8;
+			//Считаем количество элементов IEnumerable на входе
+			int tsLength = 0;
+			foreach (T item in ts)
+			{
+				tsLength++;
+			}
+			Length = tsLength;
+			int newCapacity = additCapacity + tsLength;
+			array = new T[newCapacity];
+
+			int indexer = 0;
+			foreach (T item in ts)
+			{
+				array[indexer] = item;
+				indexer++;
+			}
+		}
+
 		/// <summary>
 		/// Добавляет элемент в массив
 		/// </summary>
@@ -98,15 +166,23 @@ namespace DYNAMIC_ARRAY
 		/// <param name="ts">IEnumerable</param>
 		public void AddRange(IEnumerable<T> ts)
 		{
+			//Считаем количество элементов IEnumerable на входе
 			int tsLength = 0;
 			foreach (T item in ts)
 			{
 				tsLength++;
 			}
-
+			//Выделяем капасити и пересоздаём массив(если нужно)
 			if (Length + tsLength > Capacity)
-				Capacity = Length + tsLength;
-
+			{
+				int additionalCapacity = 8;
+				int newCapacity = Capacity + tsLength + additionalCapacity;
+				T[] buff = array;
+				array = new T[newCapacity];
+				Capacity = newCapacity;
+				buff.CopyTo(array, 0);
+			}
+			//Добавляем элементы из IEnumerable в массив 
 			int index = Length;
 			foreach (T item in ts)
 			{
@@ -128,23 +204,31 @@ namespace DYNAMIC_ARRAY
 			if (Length >= Capacity)
 				array = new T[Capacity * 2];
 			Length++;
-			for (int i = Length - 1; i > index ; i--)
+			for (int i = Length - 1; i > index; i--)
 			{
 				array[i] = array[i - 1];
 			}
 			array[index] = element;
 		}
 
+		/// <summary>
+		/// Удаляет из массива первое вхождение элемента
+		/// </summary>
+		/// <param name="elem">Элемент для удаления</param>
+		/// <returns>true, если элемент был удален. Иначе false.</returns>
 		public bool Remove(T elem)
 		{
-			for (int i = 0; i < array.Length; i++)
+			for (int i = 0; i < Length; i++)
 			{
 				if (array[i].Equals(elem))
 				{
-					for (int j = Length - 1; j < i; j++)
+					for (int j = i; j < Length - 1; j++)
 					{
-						array[j - 1] = array[j];
+						T buf = array[j + 1];
+						array[j + 1] = array[j];
+						array[j] = buf;
 					}
+					array[Length - 1] = default;
 					Length--;
 					return true;
 				}
@@ -154,12 +238,68 @@ namespace DYNAMIC_ARRAY
 
 		IEnumerator<T> IEnumerable<T>.GetEnumerator()
 		{
-			throw new NotImplementedException();
+			T[] newArray = new T[Length];
+			Array.Copy(array, newArray, Length);
+			return new DynamicArrayEnumerator<T>(newArray);
 		}
 
 		IEnumerator IEnumerable.GetEnumerator()
 		{
-			throw new NotImplementedException();
+			T[] newArray = new T[Length];
+			Array.Copy(array, newArray, Length);
+			return newArray.GetEnumerator();
+		}
+
+	}
+
+	class DynamicArrayEnumerator<T> : IEnumerator<T>
+	{
+		T[] array;
+		int position = -1;
+
+		public DynamicArrayEnumerator(T[] array)
+		{
+			this.array = array;
+		}
+
+		public T Current
+		{
+			get
+			{
+				if (position < 0 || position > array.Length)
+					throw new ArgumentOutOfRangeException();
+				else return array[position];
+			}
+		}
+
+		object IEnumerator.Current
+		{
+			get
+			{
+				if (position < 0 || position > array.Length)
+					throw new ArgumentOutOfRangeException();
+				else return array[position];
+			}
+		}
+
+		public void Dispose()
+		{
+
+		}
+
+		public bool MoveNext()
+		{
+			if (position < array.Length - 1)
+			{
+				position++;
+				return true;
+			}
+			return false;
+		}
+
+		public void Reset()
+		{
+			position = -1;
 		}
 	}
 }
